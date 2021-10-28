6a4406bf384186002f171955e65f3b7691e310a3
Jean Brito <otirbnaej@hotmail.com>
2021-10-28 15:02:11 -0300

    bd config & create user schema

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
-app.get('teste', (req, res) => {
-	console.log('teste');
-});
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running'));
